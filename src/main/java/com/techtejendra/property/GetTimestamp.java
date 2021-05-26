5eff4343166cc617ef1e45fa1ffc91e114777629
tsrana <50398037+tsrana@users.noreply.github.com>
2021-05-26 16:58:04 +0530

    Update GetTimestamp.java

--- src/main/java/com/techtejendra/property/GetTimestamp.java
+++ src/main/java/com/techtejendra/property/GetTimestamp.java
@@ -1,14 +1,11 @@
 package com.techtejendra.property;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
-
-import org.springframework.util.ResourceUtils;
 
 public class GetTimestamp {
 	
